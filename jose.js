fb1752620cc713c8d07111ec1516219a7e2ab883
josebeitia2024 <129077271+josebeitia2024@users.noreply.github.com>
2023-04-24 09:24:23 -0500

    main

---
+++ jose.js
@@ -0,0 +1,7 @@
+//Conversion larga:
+var celsius1 = prompt("Introduzca la temperatura en grados celsius: ");
+celsius1 = parseFloat(celsius1);
+
+//Conversion corta
+//var celsius1 = parseFloat(prompt("Introduzca la temperatura en grados celsius1"));
+var Farenheit = (9/5)*celsius1+32;
